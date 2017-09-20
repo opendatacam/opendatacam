d899d908c15854c55a813175ad8dadaec30fd83c
Benedikt Groß <bg@benedikt-gross.de>
2017-09-20 12:24:22 +0200

    POC calc average image (background image w/o cars)

---
+++ x-playground/average-image/average-video.py
@@ -0,0 +1,25 @@
+# http://opencvpython.blogspot.de/2012/07/background-extraction-using-running.html
+
+import cv2
+import numpy as np
+import time
+
+video = cv2.VideoCapture("C0082-47mm.mp4")
+_,frame = video.read()
+avg = np.float32(frame)
+
+while(video.isOpened()):
+    _,frame = video.read()
+    cv2.accumulateWeighted(frame,avg,0.007)
+    res = cv2.convertScaleAbs(avg)
+    cv2.imshow('avg1',res)
+
+    k = cv2.waitKey(20)
+    if k == 115: # s key
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        cv2.imwrite(timestr + ".png", res)
+    if k == 27: # ESC key
+        break
+
+cv2.destroyAllWindows()
+video.release()
