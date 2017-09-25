c06a1b3a7adc0c9bc0b336bac4f50ecdaa7b29a8
Thibault Durand <tibo.durand@gmail.com>
2017-09-25 10:00:03 +0300

    WIP: change tracker to use frame instead of time

--- tracker/main.js
+++ tracker/main.js
@@ -4,12 +4,12 @@ var Tracker = require('./Tracker');
 yolo = {};
 tracked = {};
 
-fs.readFile("../x-playground/tracker-test/C0082-47mm_detected.txt", function(err, f){
+fs.readFile("../x-playground/car-disappear/C0082-47mm-absolute.txt", function(err, f){
     var lines = f.toString().split('\n');
     lines.forEach(function(l) {
       try {
         var detection = JSON.parse(l);
-        yolo[detection.time] = detection.detections;
+        yolo[detection.frame] = detection.detections;
       } catch (e) {
         console.log('error parsing line:');
         console.log(l);
@@ -23,7 +23,7 @@ fs.readFile("../x-playground/tracker-test/C0082-47mm_detected.txt", function(err
 
     Tracker.printNbOfItemMatchedOverTime();
 
-    fs.writeFile('../x-playground/tracker-test/trackerData.json', JSON.stringify(tracked), function() {
+    fs.writeFile('../x-playground/car-disappear/trackerData.json', JSON.stringify(tracked), function() {
       console.log('tracked data wrote');
     });
 });
