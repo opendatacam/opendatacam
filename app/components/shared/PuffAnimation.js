e74a26879184dab1e74af924cc7669d870f16ab8
Thibault Durand <tibo.durand@gmail.com>
2017-10-06 10:37:26 +0300

    WIP: increase puff animation size

--- app/components/shared/PuffAnimation.js
+++ app/components/shared/PuffAnimation.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { TweenMax } from 'gsap';
 
+// const puffSize = 90;
+
 class PuffAnimation extends Component {
   
   componentDidMount() {
     this.anim = TweenMax.to(this.el, 0.5,{
-      x:-320,
+      x:-450,
       ease:SteppedEase.config(5),
       onComplete: () => this.props.removePuffAnimation(this.props.id)
     });
@@ -17,22 +19,28 @@ class PuffAnimation extends Component {
       <div 
         className="puff-animation"
         style={{
-          top:  this.props.y - 32,
-          left: this.props.x - 32
+          top:  this.props.y - 45,
+          left: this.props.x - 45
         }}
       >
-        <img 
+        <img
+          className="puff-animation-img" 
           ref={(el) => this.el = el}
-          src="/static/puff-smoke.png" 
+          src="/static/puff-smoke.svg" 
         />
         <style jsx>{`
           .puff-animation {
             position: absolute;
-            width: 64px;
-            height: 64px;
+            width: 90px;
+            height: 90px;
             overflow:hidden;
             z-index: 5;
             transform: will-change;
+          }
+
+          .puff-animation-img {
+            width: 450px;
+            height: auto;
           }
         `}</style>
       </div>
