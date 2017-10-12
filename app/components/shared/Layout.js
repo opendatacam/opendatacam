e2fe6fc1625bc1e88f43248d0d9d2da0dae85b7f
Thibault Durand <tibo.durand@gmail.com>
2017-10-12 16:21:44 +0300

    Fix: avoid safari mobile to zoom on changing orientation

--- app/components/shared/Layout.js
+++ app/components/shared/Layout.js
@@ -9,7 +9,7 @@ class Layout extends Component {
         <Head>
           <title>Traffic Cam</title>
           <meta charSet='utf-8' />
-          <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0" />
+          <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1,user-scalable=0,initial-scale=1" />
           <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
         </Head>
         {this.props.children}
