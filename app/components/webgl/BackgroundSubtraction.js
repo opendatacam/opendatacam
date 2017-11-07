90eed7886a4b6a19330c763b114f7faeeb34575c
Thibault Durand <tibo.durand@gmail.com>
2017-11-07 16:19:31 +0200

    WIP: improve background subtraction shader

--- app/components/webgl/BackgroundSubtraction.js
+++ app/components/webgl/BackgroundSubtraction.js
@@ -16,7 +16,11 @@ void main () {
   vec3 frameColor = texture2D(children, uv).rgb;
   vec3 canvas2DColor = texture2D(canvas2d, uv).rgb;
   if(canvas2DColor.r == 1.0) {
-    gl_FragColor = vec4(frameColor - averageColor, 1.0);
+    if(dot(abs(frameColor - averageColor), vec3(1)) >= 0.25) {
+      gl_FragColor = vec4(averageColor, 1.0);
+    } else {
+      gl_FragColor = vec4(frameColor, 1.0);
+    }
   } else {
     gl_FragColor = vec4(frameColor, 1.0);
   }
