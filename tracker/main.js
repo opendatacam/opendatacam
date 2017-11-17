4b6b1a83ebe19bef919e58045ee85c323a2bd87c
Thibault Durand <tibo.durand@gmail.com>
2017-11-17 15:59:20 +0200

    Evo: add feature to remove objects too large + command line arg

--- tracker/main.js
+++ tracker/main.js
@@ -4,12 +4,18 @@ var Tracker = require('./Tracker');
 yolo = {};
 tracked = {};
 
-var path = "../app/static/detections/1_prototype_video";
+var args = process.argv.slice(2);
+
+// var path = "../app/static/detections/1_prototype_video";
+var path = args[0];
+
+// Larger than 40% of the frame
+var LARGEST_ALLOWED = 1920 * 40 / 100;
 
 var DETECT_LIST = ["car", "bicycle", "truck", "motorbike"];
 
-var IGNORED_AREAS = [{"x":634,"y":1022,"w":192,"h":60},{"x":1240,"y":355,"w":68,"h":68},{"x":1295,"y":335,"w":56.00000000000001,"h":56.00000000000001},{"x":1337,"y":300,"w":57.99999999999999,"h":57.99999999999999},{"x":1378,"y":265,"w":57.99999999999999,"h":57.99999999999999},{"x":1461,"y":-2.25,"w":378,"h":282},{"x":1770,"y":825,"w":180,"h":200}];
-
+// var IGNORED_AREAS = [{"x":634,"y":1022,"w":192,"h":60},{"x":1240,"y":355,"w":68,"h":68},{"x":1295,"y":335,"w":56.00000000000001,"h":56.00000000000001},{"x":1337,"y":300,"w":57.99999999999999,"h":57.99999999999999},{"x":1378,"y":265,"w":57.99999999999999,"h":57.99999999999999},{"x":1461,"y":-2.25,"w":378,"h":282},{"x":1770,"y":825,"w":180,"h":200}];
+var IGNORED_AREAS = [];
 
 function isInsideArea(area, point) {
   const xMin = area.x
@@ -36,6 +42,15 @@ function ignoreObjectsNotToDetect(detections, objectsToDetect) {
   return detections.filter((detection) => objectsToDetect.indexOf(detection.name) > -1)
 }
 
+// clean yolo noise "large detections"
+function isTooLarge(detections) {
+  if(detections.w >= LARGEST_ALLOWED) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 fs.readFile(`${path}/rawdetections.txt`, function(err, f){
     var lines = f.toString().split('\n');
     lines.forEach(function(l) {
@@ -54,7 +69,9 @@ fs.readFile(`${path}/rawdetections.txt`, function(err, f){
       let detectionsForThisFrame = yolo[frameNb].filter((detection) => !isInsideSomeAreas(IGNORED_AREAS, detection));
       // Remove unwanted items
       detectionsForThisFrame = ignoreObjectsNotToDetect(detectionsForThisFrame, DETECT_LIST);
-      
+      // Remove objects too big
+      detectionsForThisFrame = yolo[frameNb].filter((detection) => !isTooLarge(detection));
+
       Tracker.updateTrackedItemsWithNewFrame(detectionsForThisFrame, parseInt(frameNb, 10));
       tracked[frameNb] = Tracker.getJSONOfTrackedItems();
     });
