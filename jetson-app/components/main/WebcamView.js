fe4ff4b37c8468ce13e5c8fbca3153cc25a62a37
Thibault Durand <tibo.durand@gmail.com>
2017-11-23 09:26:53 +0000

    WIP: Image webcam stream render

--- jetson-app/components/main/WebcamView.js
+++ jetson-app/components/main/WebcamView.js
@@ -1,16 +1,38 @@
 import React from 'react'
 import { connect } from 'react-redux'; 
+import { setInterval } from 'core-js/library/web/timers';
+import { clearInterval } from 'timers';
 
 class WebcamView extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      dateRefresh: new Date().getTime()
+    };
+
+    this.refresh = this.refresh.bind(this);
+  }
+
+  componentDidMount() {
+    this.refreshInterval = setInterval(() => {
+      this.refresh();
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.refreshInterval);
+  }
+
+  refresh() {
+    this.setState({ dateRefresh: new Date().getTime() });
   }
 
   render () {
     return (
       <div className="webcam-view">
-        <img src="http://192.168.1.222:8090/webcam.jpg" />
+        <img src={`http://192.168.1.222:8090/webcam.jpg?${this.state.dateRefresh}`} />
         <style jsx>{`
           @media (min-aspect-ratio: 16/9) {
             .webcam-view {
@@ -29,6 +51,12 @@ class WebcamView extends React.Component {
           img {
             width: 1280px;
             height: 720px;
+          }
+
+          .btn-refresh {
+            position: absolute;
+            top: 5px;
+            left: 5px;
           }
         `}</style>
       </div>
