c68495c09227fb71bf1d7bb70df1005241ab819b
Thibault Durand <tibo.durand@gmail.com>
2019-01-09 14:55:59 -0300

    Fix: new icons sizing

--- components/shared/BtnScreenshot.js
+++ components/shared/BtnScreenshot.js
@@ -8,7 +8,9 @@ class BtnScreenshot extends Component {
         className="btn-screenshot"
         onClick={() => this.props.onClick()}
       >
-        <img src="/static/icons/icon-screenshot.svg" />
+        <div className="btn-screenshot-icon">
+          <img src="/static/icons/icon-screenshot.svg" />
+        </div>
         <h2>Save Frame</h2>
         <style jsx>{`
           .btn-screenshot{
@@ -16,6 +18,14 @@ class BtnScreenshot extends Component {
             text-align: center;
             z-index: 2;
             cursor: pointer;
+          }
+
+          .btn-screenshot-icon {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 60px;
           }
       
           .btn-screenshot h2{
