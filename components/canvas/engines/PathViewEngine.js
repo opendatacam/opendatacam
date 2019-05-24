9ea16b55a3e5b76e64454fbf46bffc9b0a61a858
Thibault Durand <tibo.durand@gmail.com>
2019-05-24 10:39:38 -0300

    Evo: make pathfinder colors configurable

--- components/canvas/engines/PathViewEngine.js
+++ components/canvas/engines/PathViewEngine.js
@@ -1,21 +1,11 @@
 import { scaleDetection } from '../../../utils/resolution';
+import { PATHFINDER_COLORS } from '../../config.json';
 
 class PathViewEngine {
 
   constructor() {
     this.lastFrameData = [];
-    this.pathsColors = [
-      "#1f77b4",
-      "#ff7f0e",
-      "#2ca02c",
-      "#d62728",
-      "#9467bd",
-      "#8c564b",
-      "#e377c2",
-      "#7f7f7f",
-      "#bcbd22",
-      "#17becf"
-    ];
+    this.pathsColors = PATHFINDER_COLORS;
   }
 
   drawLine(context, line, color = 'green') {
