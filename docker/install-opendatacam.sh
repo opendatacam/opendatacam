d99585a761143e9fe42c7723d224a5d73f6f5c5f
Thibault Durand <tibo.durand@gmail.com>
2019-07-05 06:18:58 +0200

    Evo: remove sudo for nvidiadocker run

--- docker/install-opendatacam.sh
+++ docker/install-opendatacam.sh
@@ -117,7 +117,7 @@ else
         sudo ./run-docker.sh run -d --restart unless-stopped opendatacam/opendatacam:$VERSION-$PLATFORM
       else
         # Pull, install and run opendatacam container when docker starts (on boot with --restart unless-stopped, -d is for detached mode)
-        sudo ./run-nvidiadocker.sh run -d --restart unless-stopped opendatacam/opendatacam:$VERSION-$PLATFORM
+        ./run-nvidiadocker.sh run -d --restart unless-stopped opendatacam/opendatacam:$VERSION-$PLATFORM
       fi
 
       # Message that docker container has been started and opendatacam will be available shorty on <IP>
