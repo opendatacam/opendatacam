9680d3ebe67ff7b134deaadb2699d509ad01ea98
GEORGIOS METAXAKIS <gmetax>
2019-07-08 13:06:55 +0300

    add '--device=/dev/video0:/dev/video0` on nvidiadocker script

--- docker/run-nvidia-docker/run-nvidiadocker.sh
+++ docker/run-nvidia-docker/run-nvidiadocker.sh
@@ -3,4 +3,4 @@ DOCKER_VOLUMES+="-v $(pwd)/config.json:/var/local/opendatacam/config.json "
 # Mount a video directory for people to run on files
 DOCKER_VOLUMES+="-v $(pwd)/opendatacam_videos:/var/local/darknet/opendatacam_videos:ro "
 shift
-docker run --runtime=nvidia -p 8080:8080 -p 8090:8090 -p 8070:8070 $DOCKER_VOLUMES -v /data/db:/data/db $@
+docker run --runtime=nvidia -p 8080:8080 -p 8090:8090 -p 8070:8070 --device=/dev/video0:/dev/video0 $DOCKER_VOLUMES -v /data/db:/data/db $@
