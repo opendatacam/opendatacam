fcc37f10f4e611436b326c0956c1839f8f4c33d3
Thibault Durand <tibo.durand@gmail.com>
2019-07-10 02:49:48 -0300

    Fix: do not mount  --device=/dev/video0:/dev/video0 all time because this fails is webcam isn't connected

--- docker/run-nvidia-docker/run-nvidiadocker.sh
+++ docker/run-nvidia-docker/run-nvidiadocker.sh
@@ -3,4 +3,5 @@ DOCKER_VOLUMES+="-v $(pwd)/config.json:/var/local/opendatacam/config.json "
 # Mount a video directory for people to run on files
 DOCKER_VOLUMES+="-v $(pwd)/opendatacam_videos:/var/local/darknet/opendatacam_videos:ro "
 shift
-docker run --runtime=nvidia -p 8080:8080 -p 8090:8090 -p 8070:8070 --device=/dev/video0:/dev/video0 $DOCKER_VOLUMES -v /data/db:/data/db $@
+# If usbcam add --device=/dev/video0:/dev/video0 ... Will improve to detect and add it auto from config.json
+docker run --runtime=nvidia -p 8080:8080 -p 8090:8090 -p 8070:8070 $DOCKER_VOLUMES -v /data/db:/data/db $@
