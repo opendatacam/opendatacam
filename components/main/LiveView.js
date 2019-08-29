a9366d456adfc0956972d006135851a5a28a3617
Clair <zhiqing.clair.hu@gmail.com>
2019-08-29 15:48:04 -0700

    Revert "Updated code. Wanted to add export images of detected objects to jpeg but not working so far."
    This reverts commit 790cd73243d4b850951b41b3811baed25367b535.

--- components/main/LiveView.js
+++ components/main/LiveView.js
@@ -4,8 +4,6 @@ import { connect } from 'react-redux';
 import CanvasEngine from '../canvas/CanvasEngine';
 import { CANVAS_RENDERING_MODE } from '../../utils/constants';
 import BtnRecording from '../shared/BtnRecording';
-import SVG from 'react-inlinesvg';
-// import html2canvas from 'html2canvas';
 
 class LiveView extends PureComponent {
 
@@ -22,61 +20,11 @@ class LiveView extends PureComponent {
 
   }
 
-  // downloadFrame() {
-  //   // var f = detectionScaledOfThisFrame[0];
-  //   var canvas = CanvasEngine.canvasContext;
-  //   // var canvas = document.getElementsByClassName("canvas")[0];
-  //   console.log(canvas);
-  //   // var cropped = canvas.drawImage(canvas, 100,100,500,100);
-  //   var img = canvas.toDataURL("image/png");
-  //   // this.downloadBtn.setAttribute('href', this.hiddenCanvas.toDataURL());
-  //   // this.downloadBtn.setAttribute('download', "pathfinder.png")
-  // }
-  registerHiddenCanvas(el) {
-    this.hiddenCanvas = el;
-  }
-
-  downloadFrame() {
-    // html2canvas(document.querySelector("#capture")).then(canvas => {
-    //   document.body.appendChild(canvas)
-    // });
-    // var c = new Croppie(document.getElementById("webcam-image"));
-    // c.get();
-    this.downloadBtn.setAttribute('href', this.hiddenCanvas.toDataURL());
-    this.downloadBtn.setAttribute('download', "pathfinder.png")
-  }
-
-  clearCanvas() {
-    // Clear both hidden and visible canvas
-    this.clearVisibleCanvas();
-    this.clearHiddenCanvas();
-  }
-
   render () {
     return (
       <>
-        <CanvasEngine 
-        mode={CANVAS_RENDERING_MODE.LIVEVIEW}
-        onDomReady={(el) => this.hiddenCanvas = el}
-        registerClearCanvas={(clearCanvas) => this.clearHiddenCanvas = clearCanvas} />
+        <CanvasEngine mode={CANVAS_RENDERING_MODE.LIVEVIEW} />
         <BtnRecording />
-        {!this.props.hidden &&
-          <div className="flex fixed bottom-0 left-0 mb-2 ml-2 z-10">
-            <a
-              id="downloadFrame"
-              ref={(el) => { this.downloadBtn = el }}
-              className="btn btn-default p-0 rounded-r shadow cursor-pointer"
-              onClick={() => this.downloadFrame()}
-            >
-              <SVG 
-                className="w-10 h-10 svg-icon flex items-center" 
-                cacheGetRequests={true}
-                src={`/static/icons/ui/download.svg`} 
-                aria-label="icon take screenshot"
-              />
-            </a>
-          </div>
-        }
       </>
     )
   }
