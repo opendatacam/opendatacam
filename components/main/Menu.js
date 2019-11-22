1993dd05ae49584a560ce2f43fd4a09ef8ca435d
Georgios Metaxakis <gmetax@ics.forth.gr>
2019-11-22 13:36:56 +0200

    clickable title in menu (UI improvement)

--- components/main/Menu.js
+++ components/main/Menu.js
@@ -58,7 +58,7 @@ class Menu extends Component {
             />
           </button>
           <div className="p-5 w-full overflow-y-scroll">
-            <h3 className="mb-4 text-2xl font-bold">OpenDataCam</h3>
+            <h3 className="mb-4 text-2xl font-bold"><a className="mt-2" href="https://www.move-lab.com/project/opendatacam/" target="_blank">OpenDataCam</a></h3>
             <Toggle
               label="Counter"
               description="Count objects on active areas"
