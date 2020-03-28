49a09678a09ad34bb4fccb121fa721b7c443a787
Thibault Durand <tibo.durand@gmail.com>
2020-03-28 09:59:01 +0100

    Evo: generate up to date api documentation

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,1 +1,18 @@
-define({  "name": "OpenDataCam API",  "version": "3.0.0-alpha.1",  "description": "",  "title": "OpenDataCam API",  "template": {    "withCompare": false  },  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2020-03-25T08:46:20.949Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
+define({
+  "name": "OpenDataCam API",
+  "version": "3.0.0-alpha.1",
+  "description": "",
+  "title": "OpenDataCam API",
+  "template": {
+    "withCompare": false
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-03-28T08:58:34.762Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
