7173a21e72d421e2a14a577791d3b0785c531749
Thibault Durand <tibo.durand@gmail.com>
2020-04-30 17:39:10 +0200

    Evo: add favicon

--- components/shared/Layout.js
+++ components/shared/Layout.js
@@ -13,6 +13,10 @@ class Layout extends Component {
             name='viewport'
             content='width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1,user-scalable=0,initial-scale=1'
           />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"></link>
           <script type="text/javascript" src="/static/js/fabric.min.js" />
         </Head>
         {this.props.children}
