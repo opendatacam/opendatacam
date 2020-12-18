b5e7f613fc8e5771850e4a6ce09fe55168dbbc83
Valentin Sawadski <valentin@sawadski.eu>
2020-12-18 16:55:37 +0100

    style(spec): Fixes all eslint warnings

--- spec/scripts/YoloSimulation.spec.js
+++ spec/scripts/YoloSimulation.spec.js
@@ -1,43 +1,43 @@
 const { YoloSimulation } = require('../../scripts/YoloSimulation');
 
-describe('YoloSimulation', function () {
+describe('YoloSimulation', () => {
   let yolo = null;
 
-  beforeEach(function () {
+  beforeEach(() => {
     const yoloConfig = {
       yoloParams: {
-        data: "cfg/coco.data",
-        cfg: "cfg/yolov4-tiny.cfg",
-        weights: "yolov4-tiny.weights"
+        data: 'cfg/coco.data',
+        cfg: 'cfg/yolov4-tiny.cfg',
+        weights: 'yolov4-tiny.weights',
       },
       videoType: 'file',
       videoParams: {
-        yolo_json: "public/static/placeholder/alexeydetections30FPS.json",
-        video_file_or_folder: "public/static/placeholder/frames",
+        yolo_json: 'public/static/placeholder/alexeydetections30FPS.json',
+        video_file_or_folder: 'public/static/placeholder/frames',
         isLive: false,
         jsonFps: 20,
-        mjpgFps: 0.2
+        mjpgFps: 0.2,
       },
       jsonStreamPort: 8070,
       mjpegStreamPort: 8090,
       darknetPath: './spec/scripts/',
-      simulationStartupDelayMs: 0
+      simulationStartupDelayMs: 0,
     };
     yolo = new YoloSimulation(yoloConfig);
   });
 
-  describe('videoResolution', function () {
-    it('has 0x0 on creation', function () {
+  describe('videoResolution', () => {
+    it('has 0x0 on creation', () => {
       expect(yolo.getVideoResolution()).toEqual({ w: 0, h: 0 });
     });
 
-    it('parses emits videoresolutionevent', async function () {
+    it('parses emits videoresolutionevent', async () => {
       let emittedResolution = null;
-      yolo.on('videoresolution', (resolution) => { emittedResolution = resolution });
+      yolo.on('videoresolution', (resolution) => { emittedResolution = resolution; });
       yolo.start();
 
       // Give yolo 100ms to start the subprocess
-      await new Promise(resolve => setTimeout(resolve, 100));
+      await new Promise((resolve) => setTimeout(resolve, 100));
 
       const expectedResolution = { w: 1280, h: 720 };
       expect(yolo.getVideoResolution()).toEqual(expectedResolution);
@@ -45,19 +45,19 @@ describe('YoloSimulation', function () {
     });
   });
 
-  describe('cmd line args', function () {
+  describe('cmd line args', () => {
     const expectedConfig = {
       videoParams: {
-        yolo_json: "public/static/placeholder/alexeydetections30FPS.json",
-        video_file_or_folder: "public/static/placeholder/frames",
+        yolo_json: 'public/static/placeholder/alexeydetections30FPS.json',
+        video_file_or_folder: 'public/static/placeholder/frames',
         isLive: false,
         jsonFps: 40,
         mjpgFps: 4,
       },
       darknetStdout: false,
       jsonStreamPort: 5070,
-      mjpegStreamPort: 5090
-    }
+      mjpegStreamPort: 5090,
+    };
 
     const argsDarknetPrefix = ['detector', 'demo', 'data', 'config', 'weights'];
     const argsDarknetSuffix = [
@@ -65,17 +65,17 @@ describe('YoloSimulation', function () {
       '-dont_show',
       '-dontdraw_bbox',
       '-json_port', expectedConfig.jsonStreamPort,
-      '-mjpeg_port', expectedConfig.mjpegStreamPort
+      '-mjpeg_port', expectedConfig.mjpegStreamPort,
     ];
     const argsMissingRequiredNoDarknet = [
-      "--video_file_or_folder", expectedConfig.videoParams.video_file_or_folder,
-      "--isLive", expectedConfig.videoParams.isLive,
-      "--jsonFps", expectedConfig.videoParams.jsonFps,
-      "--mjpgFps", expectedConfig.videoParams.mjpgFps,
-      "--darknetStdout", expectedConfig.darknetStdout
+      '--video_file_or_folder', expectedConfig.videoParams.video_file_or_folder,
+      '--isLive', expectedConfig.videoParams.isLive,
+      '--jsonFps', expectedConfig.videoParams.jsonFps,
+      '--mjpgFps', expectedConfig.videoParams.mjpgFps,
+      '--darknetStdout', expectedConfig.darknetStdout,
     ];
     const argsValidNoDarknet = [
-      "--yolo_json", expectedConfig.videoParams.yolo_json,
+      '--yolo_json', expectedConfig.videoParams.yolo_json,
     ].concat(argsMissingRequiredNoDarknet);
     const argsValidDarknet = argsDarknetPrefix.concat(argsValidNoDarknet).concat(argsDarknetSuffix);
     // YoloDarknet invokes it in a weird way that we have to handle separately
@@ -97,7 +97,7 @@ describe('YoloSimulation', function () {
       '5090'];
 
     /** The parsed config object */
-    var cfg = null;
+    let cfg = null;
     /**
      * Creates a function object that will call parse with the given arguments when called
      *
@@ -107,10 +107,8 @@ describe('YoloSimulation', function () {
      *
      * @return A function that takes no arguments that will call YoloSimulation.parseCmdLine
      */
-    const invokeParser = (args) => {
-      return () => {
-        cfg = YoloSimulation.parseCmdLine(args);
-      };
+    const invokeParser = (args) => () => {
+      cfg = YoloSimulation.parseCmdLine(args);
     };
 
     it('fails on empty command line args', () => {
@@ -130,8 +128,8 @@ describe('YoloSimulation', function () {
 
           // expect default port numbers as arguments are missing
           jsonStreamPort: 8070,
-          mjpegStreamPort: 8090
-        }
+          mjpegStreamPort: 8090,
+        },
       );
     });
 
