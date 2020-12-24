f53da58100cb1d51070570745d81fd4c268f765f
Valentin Sawadski <valentin@sawadski.eu>
2020-12-24 17:01:19 +0100

    refactor(Recording): eslint refactoring and fixes

--- server/model/Recording.js
+++ server/model/Recording.js
@@ -1,22 +1,37 @@
-
-
-// Recording has methods to persist data to DB as it grows
-// The idea is never having all the data in memory as 
-// trackerHistory can be quite huge
-
+/**
+ * A single recording
+ */
 class Recording {
-  constructor (
-      dateStart,
-      dateEnd,
-      areas,
-      videoResolution,
-      filename) {
-    this.dateStart = dateStart
-    this.dateEnd = dateEnd
-    this.areas = areas
-    this.videoResolution = videoResolution
-    this.filename = filename
+  // Recording has methods to persist data to DB as it grows
+  // The idea is never having all the data in memory as
+  // trackerHistory can be quite huge
+
+  /**
+   * Creates a new Recording object
+   *
+   * See documentation API documentation for "Register areas" and "Webcam Resolution" for more
+   * information on the `areas` and `videoResolution` parameters.
+   *
+   * @constructor
+   * @param {Date} dateStart Beginning of the recording
+   * @param {Date} dateEnd End of recording
+   * @param {*} areas The recording areas used in this recording
+   * @param {*} videoResolution Resolution of the file or live camera
+   * @param {String} filename Name of the file, or `null` if running  from live camera
+   */
+  constructor(
+    dateStart,
+    dateEnd,
+    areas,
+    videoResolution,
+    filename,
+  ) {
+    this.dateStart = dateStart;
+    this.dateEnd = dateEnd;
+    this.areas = areas;
+    this.videoResolution = videoResolution;
+    this.filename = filename;
   }
 }
 
-module.exports = Recording
+module.exports = { Recording };
