bb66a31e19041e5a1470dbc9b8a1c6e085057963
Valentin Sawadski <valentin@sawadski.eu>
2021-02-05 17:10:39 +0100

    feat(scripts): Make sure scripts run outside of git
    Required for the trimmed docker images. See also #369

--- scripts/npm/build.sh
+++ scripts/npm/build.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
-REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# Try to find the repo-root through git.
+# If there's no git repository, surpress git errors and use the current directory
+REPO_ROOT=$( git rev-parse --show-toplevel 2> /dev/null || echo . )
 
 $REPO_ROOT/scripts/npm/generateapidoc.sh
 npx next build $REPO_ROOT
