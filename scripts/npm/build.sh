80e86bb98478f838d29dd765ce031ad81751dff4
Munsterlander <munsterlander@users.noreply.github.com>
2021-11-05 11:37:08 -0700

    Adding the update call to get rid of the warnings
    The current browser database is out of date and adding this to the build process ensures we have the most up-to-date.

--- scripts/npm/build.sh
+++ scripts/npm/build.sh
@@ -4,5 +4,6 @@
 # If there's no git repository, surpress git errors and use the current directory
 REPO_ROOT=$( git rev-parse --show-toplevel 2> /dev/null || echo . )
 
+npx browserslist@latest --update-db
 $REPO_ROOT/scripts/npm/generateapidoc.sh
 npx next build $REPO_ROOT
