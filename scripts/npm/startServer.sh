9f536033f0a88553e9733f9099fb617707f46d43
vsaw <hello@vsaw.net>
2023-11-28 17:45:37 +0100

    Update to NodeJS v18 Fixes #602

--- scripts/npm/startServer.sh
+++ scripts/npm/startServer.sh
@@ -4,6 +4,17 @@
 # If there's no git repository, surpress git errors and use the current directory
 REPO_ROOT=$( git rev-parse --show-toplevel 2> /dev/null || echo . )
 
+NODE_VERSION=$( npx semver $( node --version ))
+NEXT_VERSION=$( npx semver $( npx next --version ))
+if [ $(npx semver $NODE_VERSION --range "> 14") ] && [ $(npx semver $NEXT_VERSION --range "< 11") ]; then
+  echo "Warning!"
+  echo "The combination of Node v$NODE_VERSION and Next.js v$NEXT_VERSION is not supported officially."
+  echo "Setting NODE_OPTIONS=--openssl-legacy-provider as a work around"
+  echo "See also https://github.com/opendatacam/opendatacam/issues/624"
+  echo ""
+  export NODE_OPTIONS=--openssl-legacy-provider
+fi
+
 if [ -z $PORT ]; then
   PORT=8080
 fi
@@ -21,4 +32,5 @@ else
   # Explicitly generate API docs during development
   $REPO_ROOT/scripts/npm/generateapidoc.sh
 fi
+
 node $REPO_ROOT/server.js
